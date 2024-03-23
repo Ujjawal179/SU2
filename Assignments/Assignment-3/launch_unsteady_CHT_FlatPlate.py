9eafe62d435a5c4237a0e7cf8b038fbe81c504bd
Ujjawal Agrawal <72138726+Ujjawal179@users.noreply.github.com>
2024-03-23 21:21:09 +0530

    Update launch_unsteady_CHT_FlatPlate.py

--- Assignments/Assignment-3/launch_unsteady_CHT_FlatPlate.py
+++ Assignments/Assignment-3/launch_unsteady_CHT_FlatPlate.py
@@ -1,30 +1,3 @@
-#!/usr/bin/env python
-
-## \file launch_unsteady_CHT_FlatPlate.py
-#  \brief Python script to launch SU2_CFD with customized unsteady boundary conditions using the Python wrapper.
-#  \author David Thomas
-#  \version 8.0.1 "Harrier"
-#
-# SU2 Project Website: https://su2code.github.io
-#
-# The SU2 Project is maintained by the SU2 Foundation
-# (http://su2foundation.org)
-#
-# Copyright 2012-2024, SU2 Contributors (cf. AUTHORS.md)
-#
-# SU2 is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# SU2 is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with SU2. If not, see <http://www.gnu.org/licenses/>.
-
 # ----------------------------------------------------------------------
 #  Imports
 # ----------------------------------------------------------------------
@@ -107,7 +80,7 @@ def main():
     # Time iteration preprocessing
     SU2Driver.Preprocess(TimeIter)
     # Define the homogeneous unsteady wall temperature on the structure (user defined)
-    WallTemp = 293.0 + 257.0*sin(20*pi*time)
+    WallTemp = 293.0 + 257.0*sin(20*pi*time)    
     print(f"\nat time : {time}\t temp is : {WallTemp}\n")
     # Set this temperature to all the vertices on the specified CHT marker
     for iVertex in range(nVertex_CHTMarker):
